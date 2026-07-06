6fbfdb725295a5c1d457338ec2b094a95a75660d
Adarsh Gupta <Aadarshgupta7488@gmail.com>
2026-07-06 22:27:24 +0530

    Update project Section

--- src/components/portfolio/Menu.jsx
+++ src/components/portfolio/Menu.jsx
@@ -1,41 +1,57 @@
-import Work1 from "../../assets/native-jobs.png";
-import Work2 from "../../assets/cryptoverse.png";
-import Work3 from "../../assets/travel.png";
-import Work4 from "../../assets/blog-1.svg";
+import Work1 from "../../assets/audioTranscribe2.png";
+import Work2 from "../../assets/nexaAI.png";
+import Work3 from "../../assets/QuenGen.png";
+import Work4 from "../../assets/ChatApp.png";
+import Work5 from "../../assets/powerBi.png";
+import Work6 from "../../assets/QRCodeGen.png";
 
 const Menu = [
 	{
 		id: 1,
 		image: Work1,
-		title: "Coming soon...",
-		category: ["Frontend", "React"],
-		repositoryUrl: "#"
+		title: "AI Audio Transcriber & Summarizer",
+		category: ["Frontend",  "React", "Backend"],
+		url: "https://audio-transcribe-frontend-eta.vercel.app/",
+		repositoryUrl: "https://github.com/Adarsh013/audio-transcribe"
 	},
 	{
 		id: 2,
 		image: Work2,
-		title: "Coming soon...",
-		category: ["Frontend", "React"],
-		url: "#",
-		repositoryUrl: "#"
+		title: "AI Assistant & Recipe Generator",
+		category: ["Frontend", "React", "Backend"],
+		url: "https://spring-ai-demo-react.vercel.app/",
+		repositoryUrl: "https://github.com/Adarsh013/spring-ai-assistant"
 	},
 	{
 		id: 3,
 		image: Work3,
-		// title: "Travel Advisor",
-		title: "Coming soon...",
-		category: ["Frontend",  "React"],
-		url: "#",
-		repositoryUrl: "#"
+		title: "Question Paper Generator",
+		category: ["Frontend", "React", "Backend"],
+		repositoryUrl: "https://github.com/Adarsh013/question-paper-generator"
 	},
 	{
 		id: 4,
 		image: Work4,
-		title: "Coming soon...",
-		category: ["Frontend",  "React"],
-		url: "#",
-		repositoryUrl: "#"
-	}
+		title: "Chat-App",
+		category: ["Frontend", "React", "Backend"],
+		repositoryUrl: "https://github.com/Adarsh013/springboot-chat-app"
+	},
+	{
+    id: 5,
+    image: Work5,
+    title: "Mobile Sales Dashboard",
+    category: ["Power BI"],
+    repositoryUrl: "https://github.com/Adarsh013/Mobile-Sales-PowerBI"
+	},
+
+	{
+    id: 6,
+    image: Work6,
+    title: "QR Code Generator",
+    category: ["Frontend"],
+    repositoryUrl: "https://github.com/Adarsh013/QR-Code-Generator-"
+	},
+
 ];
 
 export default Menu;
